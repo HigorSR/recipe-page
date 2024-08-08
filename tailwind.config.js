6e4c38ade036a605790fb94a0d8f31bb1c55c999
Higor Rocha <higor_36rocha@hotmail.com>
2024-08-08 14:06:21 -0300

    Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "Stone-100": "hsl(30, 54%, 90%)",
+        "Stone-150": "hsl(30, 18%, 87%)",
+        "Stone-600": "hsl(30, 10%, 34%)",
+        "Stone-900": "hsl(24, 5%, 18%)",
+
+        "Brown-800": "hsl(14, 45%, 36%)",
+
+        "Rose-800": "hsl(332, 51%, 32%)",
+        "Rose-50": "hsl(330, 100%, 98%)"
+      }
     },
   },
   plugins: [],
